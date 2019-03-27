6a0caae24a9b96ce73c55afc8e33d28f235b7f54
Tahseen1234 <48979160+Tahseen1234@users.noreply.github.com>
2019-03-27 14:38:25 +0530

    ...
    ...

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const sectionStyle = {
   flex: 1,
   zIndex: 0,
   postion: "relative",
-  backgroundImage: `url('https://images.pexels.com/photos/1516648/pexels-photo-1516648.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=250')`
+  backgroundImage: `url('https://images.unsplash.com/photo-1471864190281-a93a3070b6de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')`
 };
 
 class App extends React.Component {
@@ -26,15 +26,24 @@ class App extends React.Component {
                   postion: "absolute",
                   top: `${window.innerHeight / 2 - 10}px`,
                   left: `${window.innerWidth / 2 - 100}px`,
-                  textTransform:'none'
+                  textTransform: "none"
                 }}
-                
                 variant="contained"
-                color="secondary">
-                <Typist hideWhenDone='true'>
-                  <span >WELCOME TO <span style={{color:'black',fontSize:15}}>HealthEasy</span> </span>
-                  <Typist.Backspace count={22} delay={200} hideWhenDone='true'  />
-                  <span>CLICK HERE  TO START </span>
+                color="secondary"
+              >
+                <Typist hideWhenDone="true">
+                  <span>
+                    WELCOME TO{" "}
+                    <span style={{ color: "black", fontSize: 15 }}>
+                      HealthEasy
+                    </span>{" "}
+                  </span>
+                  <Typist.Backspace
+                    count={22}
+                    delay={200}
+                    hideWhenDone="true"
+                  />
+                  <span>CLICK HERE TO START </span>
                 </Typist>
               </Button>
             </Link>
